94568e48ed65114868123c23cff43f977c1bce88
OnkarDsharma <onkareshwarsharma7om@gmail.com>
2026-03-14 15:58:40 +0530

    Disable Prisma runtime in JSON storage mode

--- apps/api/src/db/client.js
+++ apps/api/src/db/client.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const storageMode = (process.env.STORAGE_MODE || "json").toLowerCase();
+
+const prisma = storageMode === "sqlite"
+	? new PrismaClient()
+	: new Proxy({}, {
+			get() {
+				throw new Error("Prisma is disabled when STORAGE_MODE is not sqlite");
+			}
+		});
 
 module.exports = { prisma };
